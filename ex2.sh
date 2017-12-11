7bb0004ce31521244c43e4eb5a50d24bdb1ff3b4
pengpeng <351578076@qq.com>
2017-12-11 13:34:03 +0800

    shell study

---
+++ ex2.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+#yuansuan
+expr 10+3
+num1=10
+num2=3
+echo "num1+num2=$[$num1+$num2]"
+
---
+++ ex4.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#panduan 
+num1=100
+num2=200
+if test $num1 -eq $num2
+then 
+  echo num1=num2
+else 
+  echo num1 no= num2
+fi
